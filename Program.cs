d3a00d37c3bd01cefb0f2ec3832924aea87aaaac
Ayxan23 <ayxan042223@gmail.com>
2022-11-11 04:44:32 +0400

    Tasks

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Task1
+int number = 6;
+int toplama = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    toplama = toplama + i; //toplama += 1;
+}
+Console.WriteLine(toplama);
+
+
+// Task2
+int reqem = 5;
+int factorial = 1;
+
+for (int i = 1; i <= reqem; i++)
+{
+    factorial = factorial * i; //factorial *= 1;
+}
+Console.WriteLine(factorial);
+
+
+// Task3
+float eded = 30f;
+
+while (eded > 1)
+{
+    eded = eded / 2; //eded /= 2; 
+}
+if (eded == 1)
+{
+    Console.WriteLine("Ikinin quvvet ustudur");
+}
+else 
+{
+    Console.WriteLine("Ikinin quvvet ustu deyil");
+}
